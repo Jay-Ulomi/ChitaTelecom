39241b5480b422dfbbefa69d425f628a5f33c410
Jay-Ulomi <jayulomi160@gmail.com>
2025-01-27 16:49:05 +0300

    add FAQ routes and views, update contact information, and implement error handling middleware

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const serviceRoutes = require('./routes/serviceRoutes');
 const contactRoutes = require('./routes/contactRoutes');
 const planRoutes = require('./routes/planRoutes');
 const aboutRoutes = require('./routes/aboutRoutes');
+const fqnRoutes = require('./routes/fqnRoutes');
 
 
 // Middleware to parse form data
@@ -33,6 +34,12 @@ app.use('/service', serviceRoutes);
 app.use('/contact', contactRoutes);
 app.use('/plan', planRoutes);
 app.use('/about', aboutRoutes);
+app.use('/fqn', fqnRoutes);
+
+// Error handling middleware
+app.use((req, res, next) => {
+    res.status(404).send("Sorry can't find that!")
+  });
 
 // Start the server
 app.listen(PORT, () => {
